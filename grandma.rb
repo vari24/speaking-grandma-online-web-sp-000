220ecbfab8117efba8e0646817ea6bb78e27d29a
Sara Varisara <varisara.n@gmail.com>
2019-01-14 14:40:49 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,14 +1,14 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma
-  puts "Hi Nana, how are you?"
+  "Hi Nana, how are you?"
   return "HUH?! SPEAK UP, SONNY!"
-  puts "Hi!"
+  "Hi!"
   return "HUH?! SPEAK UP, SONNY!"
-  puts "WHAT DID YOU EAT TODAY?"
+  "WHAT DID YOU EAT TODAY?"
   return "NO, NOT SINCE 1938!"
-  puts "WHAT?"
+  "WHAT?"
   return "NO, NOT SINCE 1938!"
-  puts "I LOVE YOU GRANDMA!"
+  "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
   end
 end
