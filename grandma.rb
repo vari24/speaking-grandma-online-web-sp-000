05eee8ce4d118cba956ffe012e1e8b4d0f8abccf
Sara Varisara <varisara.n@gmail.com>
2019-01-14 14:48:04 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,13 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(sentence)
-  if sentence == "Hi Nana, how are you?"
-  return "HUH?! SPEAK UP, SONNY!"
-elsif sentence == "Hi!"
-  return "HUH?! SPEAK UP, SONNY!"
-elsif sentence == "WHAT DID YOU EAT TODAY?"
-  return "NO, NOT SINCE 1938!"
+  if sentence == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif sentence == "Hi!"
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif sentence == "Hi!"
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif sentence == "WHAT DID YOU EAT TODAY?"
+    return "NO, NOT SINCE 1938!"
 elsif sentence == "WHAT?"
   return "NO, NOT SINCE 1938!"
 else sentence == "I LOVE YOU GRANDMA!"
@@ -13,7 +15,7 @@ else sentence == "I LOVE YOU GRANDMA!"
 end
 end
 
-speak_to_grandma(sentence)
+speak_to_grandma("HELLO")
 
 
 
