c7acfa94fb3ef1e30e090e89ad5ba85eda999f72
Sara Varisara <varisara.n@gmail.com>
2019-01-14 14:34:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,24 @@
 # Write a speak_to_grandma method.
+def speak_to_grandma
+  if "Hi Nana, how are you?"
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif "Hi!"
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif "WHAT DID YOU EAT TODAY?"
+    return "NO, NOT SINCE 1938!"
+  elsif "WHAT?"
+    return "NO, NOT SINCE 1938!"
+  else "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  end
+end
+
+    
+    
+    
+    
+
+    
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
