dd888e2381187ac4e57e542a1a2f68dcca7416ab
Sara Varisara <varisara.n@gmail.com>
2019-01-14 15:00:11 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.downcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.downcase
