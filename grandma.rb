d9d54a9900c8a83f3efdf06374aa3ae109f4eb1a
Sara Varisara <varisara.n@gmail.com>
2019-01-14 14:59:50 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -7,7 +7,7 @@ def speak_to_grandma(phrase)
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.downcase
     return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == phrase.uppercase
+  elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
   else
     return "NO, NOT SINCE 1938!"
